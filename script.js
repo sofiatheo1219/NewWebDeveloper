e67fae5398c48cafd02ae0bae4d2d1a83068db34
sofiatheo1219 <itadakimasu1218@gmail.com>
2024-12-04 23:00:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 
+window.onload = () => {
+    document.body.style.opacity = 1;
+    document.body.style.transition = "opacity 1s ease-in-out";
+};
 
-const display = document.getElementById("display");
 
-function appendToDisplay(input){
-  display.value += input;
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
 
-function clearDisplay(){
-  display.value = "";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const galleryImages = document.querySelectorAll('.gallery-image');
+    const galleryBtn = document.getElementById('galleryBtn');
 
-function calculate(){
-  try{
-    display.value = eval(display.value);
-  }
-  catch(MissyouJmee) {
-      display.value = "Miss na kita!";
-  }
-}
+    
+    function changeImage(imageId) {
+        const image = document.getElementById(imageId);
+        if (image) {
+           
+            image.src = "new-image.jpg"; 
+        }
+    }
+
+    
+    function changeGalleryImage() {
+        const randomImage = Math.floor(Math.random() * galleryImages.length);
+        galleryImages[randomImage].src = "new-image.jpg"; 
+    }
+
+    
+    galleryBtn.addEventListener('click', changeGalleryImage);
+});
